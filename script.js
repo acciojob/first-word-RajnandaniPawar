a97d24b1fd0cfdbe74425e3dce93675276bec9ad
RajnandaniPawar <164873503+RajnandaniPawar@users.noreply.github.com>
2024-07-16 17:25:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function firstWord(s) {
   // your code here
-	    str = str.trim();
+	    s = s.trim();
 
     // Find the index of the first space
-    let firstSpaceIndex = str.indexOf(' ');
+    let firstSpaceIndex = s.indexOf(' ');
 
     // If there is no space, return the entire string
     if (firstSpaceIndex === -1) {
-        return str;
+        return s;
     }
 
     // Return the substring up to the first space
-    return str.substring(0, firstSpaceIndex);
+    return s.substring(0, firstSpaceIndex);
 
 }
 
